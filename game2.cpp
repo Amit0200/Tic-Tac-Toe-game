b8096c355031d62bf3ddcc67da5fd2a1ad545eb7
Amit Gupta <116447781+Amit0200@users.noreply.github.com>
2023-12-27 18:51:24 -0600

    Update game2.cpp

--- game2.cpp
+++ game2.cpp
@@ -6,10 +6,26 @@ void NewBoard(char *spaces){
   std::cout << "     |     |     " << std::endl;
   std::cout << "  " << spaces[0] << " | " << spaces[1] << " | " << spaces[2] << " | " << std::endl;
   std::cout << "_____|_____|_____" << std::endl;
-  
+  std::cout << "     |     |     " << std::endl;
+  std::cout << "  " << spaces[3] << " | " << spaces[4] << " | " << spaces[5] << " | " << std::endl;
+  std::cout << "_____|_____|_____" << std::endl;
+  std::cout << "     |     |     " << std::endl;
+  std::cout << "  " << spaces[6] << " | " << spaces[7] << " | " << spaces[8] << " | " << std::endl;
+  std::cout << "     |     |     " << std::endl;
+  std:endl;
+
 }
 void playerMove(char *spaces, char player){
-  
+  int number;
+  do{
+    std::cout << "Enter a spot to place a marker from 1 -9: ";
+    std::cin >> number;
+    number --;
+    if(spaces[number] == ' '){
+      spaces[number] = player;
+      break;
+    }
+  }while(!number > 0 || !number < 8);
 }
 void computerMove(char *spaces, char computer){
   
@@ -29,6 +45,12 @@ int main(){
   bool running = true;
 
   NewBoard(spaces);
+
+  while(running){
+
+    playerMove(spaces, player);
+    drawBoard(spaces);
+  }
 
   return 0;
 }
